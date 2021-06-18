d0efbfa43663e827244a1036e44278210f6c621f
Gurdeepak Sidhu <gurdeepaksidhu@yahoo.com>
2021-06-18 15:50:07 -0700

    first commit

---
+++ _build/jupyter_execute/final_report.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Meets Government of Canada Web Services
+# 
+# ***By Anita Li, Deepak Sidhu, Jianru Deng, Sakshi Jain***<br>
+# *June 28th, 2021*
+# 
+# 
+
+# ## Final Report
+# 
+# ### 1. Executive Summary
+# 
+# 
+# 
+
+# 
+# TODO
+
+# ### 2. Introduction
+# 
+# 
+# 
+
+# TODO
+# 
+
+# ### 3. Data Science Methods
+# 
+
+# #### 3.1 Jupyter Book
+# 
+# 
+
+# #### 3.2 Time Series Forecasting
+# 
+# TODO
+
+# #### 3.3 Prediction on Web Satisfaction 
+# 
+# TODO
+# 
+
+# #### 3.4 Survey Clustering
+# 
+# TODO
+# 
+# 
+
+# 
+# ### 4. Data Product and Results
+# 
+
+# TODO
+
+# ### 5. Conclusions and Recommendations
+
+# TODO
+
+# ### 6. Bibliography
+# 
+# ```{bibliography} references.bib
+# ```
